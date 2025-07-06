b4d8d78e8df4c292a22512244c72e5ac26a83122
rosenrot00 <121129193+rosenrot00@users.noreply.github.com>
2025-07-06 06:31:57 +0200

    Update __init__.py

--- components/modbus_bridge/__init__.py
+++ components/modbus_bridge/__init__.py
@@ -1,22 +1,26 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.components import uart
-from esphome.const import CONF_ID, CONF_DEBUG
+from esphome.const import CONF_ID, CONF_DEBUG, CONF_UART_ID
+
+DEPENDENCIES = ["uart"]
 
 modbus_bridge_ns = cg.esphome_ns.namespace('modbus_bridge')
 ModbusBridgeComponent = modbus_bridge_ns.class_('ModbusBridgeComponent', cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ModbusBridgeComponent),
-    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     cv.Optional("tcp_port", default=502): cv.port,
-    cv.Optional("debug", default=False): cv.boolean,
+    cv.Optional(CONF_DEBUG, default=False): cv.boolean,
 }).extend(cv.polling_component_schema("60s"))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    uart_comp = await cg.get_variable(config["uart_id"])
-    cg.add(var.set_uart_id(uart_comp))
+
+    uart_comp = await cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_uart(uart_comp))
+
     cg.add(var.set_tcp_port(config["tcp_port"]))
-    cg.add(var.set_debug(config["debug"]))
+    cg.add(var.set_debug(config[CONF_DEBUG]))
