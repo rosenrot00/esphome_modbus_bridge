6f455e0214314337ccd7216e3a5c763639634117
rosenrot00 <121129193+rosenrot00@users.noreply.github.com>
2025-08-02 11:28:39 +0200

    further optimizations

--- components/modbus_bridge/modbus_bridge.h
+++ components/modbus_bridge/modbus_bridge.h
@@ -32,8 +32,8 @@ class ModbusBridgeComponent : public Component {
   void set_uart_id(uart::UARTComponent *uart) { uart_ = uart; }
   void set_tcp_port(uint16_t port) { tcp_port_ = port; }
   void set_tcp_poll_interval(uint32_t interval_ms) { tcp_poll_interval_ms_ = interval_ms; }
-  void set_client_timeout(uint32_t timeout_ms) { client_timeout_ms_ = timeout_ms; }
-  void set_response_timeout(uint32_t ms) { modbus_response_timeout_ms_ = ms; }
+  void set_tcp_client_timeout(uint32_t timeout_ms) { tcp_client_timeout_ms_ = timeout_ms; }
+  void set_rtu_response_timeout(uint32_t timeout);
   void set_debug(bool debug);
 
   void setup() override;
@@ -46,10 +46,10 @@ class ModbusBridgeComponent : public Component {
   uint16_t tcp_port_{502};
   bool debug_{false};
   uint32_t tcp_poll_interval_ms_{50};
-  uint32_t client_timeout_ms_{30000};
-  uint32_t modbus_response_timeout_ms_{1000};
-  uint32_t inactivity_timeout_ms_{20};
-
+  uint32_t rtu_poll_interval_ms_{10};
+  uint32_t tcp_client_timeout_ms_{60000};
+  uint32_t rtu_inactivity_timeout_ms_{20};
+  uint32_t rtu_response_timeout_ms_{3000};
 
   bool polling_active_{false};
   void start_uart_polling_();
