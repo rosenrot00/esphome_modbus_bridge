36d6cee75cf360d0251b88406931c977bed7ffa8
rosenrot00 <121129193+rosenrot00@users.noreply.github.com>
2025-08-02 11:29:33 +0200

    further optimizations

--- components/modbus_bridge/__init__.py
+++ components/modbus_bridge/__init__.py
@@ -5,8 +5,8 @@ from esphome.const import CONF_ID
 
 CONF_TCP_PORT = "tcp_port"
 CONF_TCP_POLL_INTERVAL = "tcp_poll_interval"
-CONF_CLIENT_TIMEOUT = "client_timeout"
-CONF_RESPONSE_TIMEOUT = "response_timeout"
+CONF_TCP_CLIENT_TIMEOUT = "tcp_client_timeout"
+CONF_RTU_RESPONSE_TIMEOUT = "rtu_response_timeout"
 
 modbus_bridge_ns = cg.esphome_ns.namespace('modbus_bridge')
 ModbusBridgeComponent = modbus_bridge_ns.class_('ModbusBridgeComponent', cg.Component)
@@ -16,8 +16,8 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
     cv.Optional(CONF_TCP_PORT, default=502): cv.port,
     cv.Optional(CONF_TCP_POLL_INTERVAL, default=50): cv.positive_int,
-    cv.Optional(CONF_CLIENT_TIMEOUT, default=30000): cv.positive_int,
-    cv.Optional(CONF_RESPONSE_TIMEOUT, default=1000): cv.positive_int,
+    cv.Optional(CONF_TCP_CLIENT_TIMEOUT, default=60000): cv.positive_int,
+    cv.Optional(CONF_RTU_RESPONSE_TIMEOUT, default=3000): cv.positive_int,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -28,5 +28,5 @@ async def to_code(config):
     cg.add(var.set_uart_id(uart_comp))
     cg.add(var.set_tcp_port(config[CONF_TCP_PORT]))
     cg.add(var.set_tcp_poll_interval(config[CONF_TCP_POLL_INTERVAL]))
-    cg.add(var.set_client_timeout(config[CONF_CLIENT_TIMEOUT]))
-    cg.add(var.set_response_timeout(config[CONF_RESPONSE_TIMEOUT]))
+    cg.add(var.set_tcp_client_timeout(config[CONF_TCP_CLIENT_TIMEOUT]))
+    cg.add(var.set_rtu_response_timeout(config[CONF_RTU_RESPONSE_TIMEOUT]))
